4d60272cbb5e23f520636888f88611abce63c4d5
RuchitaP99 <ruchitapalkar007@gmail.com>
2022-03-22 19:25:54 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Age from './components/Age'
+import AgeCalculator from './components/AgeCal'
+
+function App() {
+  return (
+    <div>
+      {/* <Age /> */}
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
